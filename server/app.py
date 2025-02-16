c38b614c71db1704d12b84a32207776888bf6d64
Emily Ho <hoemi@classwork.engr.oregonstate.edu>
2025-02-16 12:17:09 -0800

    create server folder and setup

---
+++ server/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, json, redirect
+from flask_mysqldb import MySQL
+from flask import request
+import os
+
+app = Flask(__name__)
+
+mysql = MySQL(app)
+
+
+# Routes
+@app.route('/')
+def root():
+    query = "SELECT * FROM diagnostic;"
+    query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for yourONID!")';
+    query4 = 'SELECT * FROM diagnostic;';
+    cur = mysql.connection.cursor()
+    cur.execute(query1)
+    cur.execute(query2)
+    cur.execute(query3)
+    cur.execute(query4)
+    results = cur.fetchall()
+
+    return "<h1>MySQL Results</h1>" + str(results[0])
+
+
+# Listener
+if __name__ == "__main__":
+
+    #Start the app on port 3000, it will be different once hosted
+    app.run(port=3000, debug=True)
