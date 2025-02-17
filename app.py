757a677550d803d8df37de6065bde6848cae3ea4
Emily Ho <hoemi@classwork.engr.oregonstate.edu>
2025-02-17 07:59:41 -0800

    set up server and db connection

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, json
+import os
+import database.db_connector as db
+
+# Configuration
+app = Flask(__name__)
+db_connection = db.connect_to_database()
+
+
+# Routes
+@app.route('/')
+def root():
+    return "Hello, World!"
+
+
+# Listener
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 55241))
+    app.run(port=port, debug=True)
