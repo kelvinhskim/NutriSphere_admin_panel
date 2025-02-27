aa3c06f93d767b20a5404b52944eca6483aa54d5
Emily Ho <hoemi@classwork.engr.oregonstate.edu>
2025-02-26 23:19:15 -0800

    add test GET route

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,21 @@
 from flask import Flask
 import os
 import database.db_connector as db
+from flask_cors import CORS
 
 # Configuration
 app = Flask(__name__)
 db_connection = db.connect_to_database()
+cors = CORS(app, origins='*')
 
 # Routes 
 @app.route('/')
 def root():
-    return "Hello Bloop!!"
+    return "Hello World!!"
+
+@app.get('/api/users')
+def users():
+    return "TA-DA! This is the users route"
 
 # Listener
 if __name__ == "__main__":
