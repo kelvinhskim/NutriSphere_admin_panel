f41788d53eb3ea118eebee8c54ffae4f206080ff
Emily Ho <hoemi@classwork.engr.oregonstate.edu>
2025-02-27 18:37:42 -0800

    update error with key

--- frontend/src/components/users/UsersTableRow.jsx
+++ frontend/src/components/users/UsersTableRow.jsx
@@ -6,8 +6,8 @@ const UsersTableRow = ({ user }) => {
             <td>{user.email}</td>
             <td>{user.dailyCalorieGoal}</td>
             <td>
-                <button class="edit-btn">✏️ Edit</button>
-                <button class="delete-btn">❌ Delete</button>
+                <button className="edit-btn">✏️ Edit</button>
+                <button className="delete-btn">❌ Delete</button>
             </td>
         </tr>
     )
