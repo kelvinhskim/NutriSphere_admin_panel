73b7c4c9ceb15f3e832d3b3d223c5b4008082df0
Emily Ho <hoemi@classwork.engr.oregonstate.edu>
2025-02-27 18:39:17 -0800

    add routing for POST request

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, json
+from flask import Flask, json, request, redirect, jsonify
 import os
 import database.db_connector as db
+from flask_mysqldb import MySQL
 from flask_cors import CORS
 
 # Configuration
@@ -13,13 +14,29 @@ cors = CORS(app, origins='*')
 def root():
     return "Hello World!!"
 
-@app.get('/api/users')
+@app.route('/api/users', methods=['POST', 'GET'])
 def users():
-    """Fetches all users from the database and displays them."""
-    query = 'SELECT * FROM Users;'
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = json.dumps(cursor.fetchall())
-    return results
+    print('REQ METHOD', request.method)
+    # Insert a new user into the Users entity
+    if request.method == 'POST':
+        username = request.json.get('username')
+        email = request.json.get('email')
+        dailyCalorieGoal = request.json.get('dailyCalorieGoal')
+        print('POST USER', username, email, dailyCalorieGoal)
+        query = 'INSERT INTO Users (username, email, dailyCalorieGoal) VALUES (%s, %s, %s);'
+        # cur = mysql.connection.cursor()
+        # cur.execute(query, (username, email, dailyCalorieGoal))
+        db.execute_query(db_connection=db_connection, query=query, query_params=(username, email, dailyCalorieGoal))
+        db_connection.commit()
+        return redirect("/api/users")
+
+    # Grabs all users data to send to frontend browse users table
+    if request.method == "GET":
+        query = 'SELECT userID, username, email, dailyCalorieGoal FROM Users ORDER BY username;'
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = json.dumps(cursor.fetchall())
+        return results
+
 
 # Listener
 if __name__ == "__main__":
