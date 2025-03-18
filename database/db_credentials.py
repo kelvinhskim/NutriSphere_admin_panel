d72c329933025fc7f67ba862d1acd210df2a7455
HyunSeok Kim <kimh22@classwork.engr.oregonstate.edu>
2025-03-17 23:35:47 -0700

    Removed credentials from db_credentials.py

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -1,4 +1,4 @@
 host = "classmysql.engr.oregonstate.edu"
-user = "cs340_kimh22"  # Your OSU CS340 username
-passwd = "0612"         # Your last 4-digit ONID ID
-db = "cs340_kimh22"     # Your OSU CS340 database name
+user = "cs340_xxxx"  # Your OSU CS340 username
+passwd = "XXXX"         # Your last 4-digit ONID ID
+db = "cs340_xxxx"     # Your OSU CS340 database name
